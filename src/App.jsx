0c95d2fbab0ac8c0d04517c1885c058d2b7cf004
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-01-25 11:57:15 +0100

    dataFetching and react logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import {useState} from 'react'
+import {useEffect, useReducer} from 'react'
 import './App.css'
 import Header from "./components/Header.jsx";
 import MainComponent from "./components/MainComponent.jsx";
 
+const initialState = {
+    questions: [],
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: 'loading',
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived':
+            return {
+                ...state,
+                questions: action.payload,
+                status: 'ready'
+            }
+        case 'dataFailed':
+            return {
+                ...state,
+                status: 'error',
+            }
+        default:
+            throw new Error('Action unknown');
+    }
+}
+
 function App() {
-    const [count, setCount] = useState(0)
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        async function DataFecthing() {
+            try {
+                const resp = await fetch(`http://localhost:8000/questions`);
+                const data = await resp.json();
+                dispatch({type: 'dataReceived', payload: data})
+            } catch (error) {
+                dispatch({type: 'dataFailed'});
+            } finally {
+                console.log(`All happened correctly!`)
+            }
+        }
+
+        DataFecthing();
+    }, []);
 
     return (
         <>
